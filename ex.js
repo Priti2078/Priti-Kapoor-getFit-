4400871dcca4240066e5fa28c56c0c5b5bb0da3a
Priti2078 <kapoorpriti18@gmail.com>
2022-11-16 11:38:44 +0530

    last

--- ex.js
+++ ex.js
@@ -20,14 +20,35 @@ function myFunction() {
     if(e < 1) {
         indexValue = img.length
     }
-    for(i = 1; i < img.length; i++) {
-        img[i].style.display = "none";
-    }
     img[indexValue - 1].style.display = "block";
   }
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 
-
- 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let img = document.getElementsByClassName("img");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    img[i].style.display = "block";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
